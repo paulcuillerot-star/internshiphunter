e5235a3af99bba098e1950b1345980da4dc2706b
paulcuillerot-star <paul.cuillerot@gmail.com>
2026-06-01 20:48:36 +0200

    Configure Sentry build integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import { withSentryConfig } from "@sentry/nextjs";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  silent: true,
+  widenClientFileUpload: true,
+  hideSourceMaps: true,
+  disableLogger: true
+});
