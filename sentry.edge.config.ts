f55f8ffa204e938a29f881f658bc859dafcd3a84
paulcuillerot-star <paul.cuillerot@gmail.com>
2026-06-01 20:49:19 +0200

    Add Sentry edge config

---
+++ sentry.edge.config.ts
@@ -0,0 +1,10 @@
+import * as Sentry from "@sentry/nextjs";
+
+const dsn = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+Sentry.init({
+  dsn,
+  environment: process.env.SENTRY_ENVIRONMENT ?? process.env.VERCEL_ENV ?? process.env.NODE_ENV,
+  enabled: Boolean(dsn),
+  tracesSampleRate: 0.1
+});
