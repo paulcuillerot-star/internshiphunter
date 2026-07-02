a7a6d304b7f864218408c87bef01611d0e3e6285
paulcuillerot-star <paul.cuillerot@gmail.com>
2026-07-02 10:58:21 +0200

    Add shared direct application host constants
    Add shared direct application host constants

---
+++ lib/jobSourceHosts.ts
@@ -0,0 +1,15 @@
+export const directApplicationHosts = [
+  "greenhouse.io",
+  "lever.co",
+  "workable.com",
+  "teamtailor.com",
+  "smartrecruiters.com",
+  "ashbyhq.com",
+  "factorialhr.com",
+  "myworkdayjobs.com",
+  "workdayjobs.com",
+  "bamboohr.com",
+  "recruitee.com",
+  "personio.com",
+  "homerun.co"
+];
